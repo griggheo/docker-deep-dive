0dd7baf6e12d5da250a9aaff0e90abfe397cd4bf
Grig Gheorghiu <grig@codepraxis.io>
2022-10-11 05:09:26 +0000

    docker history and docker inspect examples + getting image size

--- oci_examples/0_docker_image_inspect.sh
+++ oci_examples/0_docker_image_inspect.sh
@@ -11,7 +11,15 @@ ghcr.io/codepraxis-io/spring-music:2.0.0-buildx-distroless-java17-debian11
 ghcr.io/codepraxis-io/spring-music:2.0.0-cnb-java11"
 
 for IMAGE in $IMAGES; do
-	echo Running: docker image history $IMAGE
-	docker image history $IMAGE
-	#docker image history --no-trunc $IMAGE
+	echo Running: docker inspect $IMAGE
+	docker inspect $IMAGE
+done
+
+
+for IMAGE in $IMAGES; do
+	echo Getting size for $IMAGE
+	size=$(docker inspect $IMAGE | jq '.[].Size')
+	echo size=$size
+	size_in_mb=$((size / (1024*1024)))
+	echo size_in_mb=$size_in_mb MB
 done
