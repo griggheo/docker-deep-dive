497cd98eeec7756bb2ba1ba28a41a6803b01c56f
Grig Gheorghiu <grig@gheorghiu.net>
2022-10-23 15:27:25 +0000

    split out sarif inspection script

--- scan_result_parsing_examples/grype_syft_sbom.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-SBOM=syft_sbom.json
-REPORT=grype_report.json
-grype -o json --file $REPORT sbom:$SBOM
--- scan_result_parsing_examples/trivy_sarif.sh
+++ scan_result_parsing_examples/trivy_sarif.sh
@@ -7,27 +7,3 @@ IMAGE=golang:1.16-alpine
 REPORT=trivy_report.sarif
 
 trivy image --format sarif -o $REPORT $IMAGE
-
-echo Scan tool info
-cat $REPORT |jq '.runs[0].tool.driver.fullName, .runs[0].tool.driver.informationUri,.runs[0].tool.driver.name, .runs[0].tool.driver.version'
-echo
-
-echo CVE list
-cat $REPORT |jq '.runs[0].tool.driver.rules[].id'
-echo
-
-echo CVE + URL
-cat $REPORT |jq '.runs[0].tool.driver.rules[]|.id,.helpUri'
-echo
-
-echo CVE + URL + help text
-cat $REPORT |jq '.runs[0].tool.driver.rules[]|.id,.helpUri,.help.text'
-echo
-
-echo CVE + severity
-cat $REPORT |jq '.runs[0].tool.driver.rules[]|.id,.properties."security-severity"'
-echo
-
-echo Select records with severity score greater than 7
-cat $REPORT |jq '.runs[0].tool.driver.rules[] | select(.properties."security-severity">"7")' | jq ".id,.helpUri,.fullDescription.text"
-echo
