56d8565c95d2284c34cafc775137a2170ae4ee1f
Grig Gheorghiu <grig@gheorghiu.net>
2022-10-29 04:12:00 +0000

    podman modifications

--- install_scripts/install_crane.sh
+++ install_scripts/install_crane.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-CRANE_VERSION=0.11.0
+CRANE_VERSION=0.12.0
 
 mkdir -p /tmp/crane
 cd /tmp/crane 
 curl -sL "https://github.com/google/go-containerregistry/releases/download/v${CRANE_VERSION}/go-containerregistry_Linux_x86_64.tar.gz" > go-containerregistry.tar.gz
+
+# verify SLSA attestation
+#curl -sL https://github.com/google/go-containerregistry/releases/download/v${CRANE_VERSION}/attestation.intoto.jsonl > provenance.intoto.jsonl
+#slsa-verifier -artifact-path go-containerregistry.tar.gz -provenance provenance.intoto.jsonl -source github.com/google/go-containerregistry -tag "v${CRANE_VERSION}"
+
 tar xvfz go-containerregistry.tar.gz
 sudo mv *crane /usr/local/bin 
 crane version 
 gcrane version
 cd /tmp 
 rm -rf crane
+
