3dff6dcdce690b1d4d984390d4b5086c7e5de5ba
Grig Gheorghiu <grig@gheorghiu.net>
2022-11-12 21:25:30 +0000

    example of verifying google distroless images

---
+++ cosign_examples/40_cosign_verify_google_distroless.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+GOOGLE_COSIGN_PUB_KEY=google_cosign.pub
+rm -rf cosign.pub
+wget https://raw.githubusercontent.com/GoogleContainerTools/distroless/main/cosign.pub
+mv cosign.pub $GOOGLE_COSIGN_PUB_KEY
+
+GOOGLE_DISTROLESS_IMAGES='
+gcr.io/distroless/static-debian11
+gcr.io/distroless/base-debian11
+gcr.io/distroless/cc-debian11
+gcr.io/distroless/python3-debian11
+gcr.io/distroless/java-base-debian11
+gcr.io/distroless/java11-debian11
+gcr.io/distroless/java17-debian11
+gcr.io/distroless/nodejs14-debian11
+gcr.io/distroless/nodejs16-debian11
+gcr.io/distroless/nodejs18-debian11'
+
+TAGS='
+latest
+nonroot
+debug
+debug-nonroot'
+
+for IMAGE in $GOOGLE_DISTROLESS_IMAGES; do
+	for TAG in $TAGS; do
+		echo Verifying $IMAGE:$TAG
+		cosign verify --key $GOOGLE_COSIGN_PUB_KEY $IMAGE:$TAG
+		echo
+	done
+done
+
+# found an issue!
+#Verifying gcr.io/distroless/python3-debian11:debug
+#Error: no matching signatures:
+#
+#main.go:62: error during command execution: no matching signatures:
