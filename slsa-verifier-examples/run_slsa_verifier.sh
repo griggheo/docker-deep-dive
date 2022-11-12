1e0ef6cf50c7a8f12cb18616ecb82a63743504d9
Grig Gheorghiu <grig@gheorghiu.net>
2022-11-12 04:58:06 +0000

    example of running slsa-verifier against binary released via slsa github action

---
+++ slsa-verifier-examples/run_slsa_verifier.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SOURCE_URI=github.com/codepraxis-io/katana
+SOURCE_TAG=v1.0.0
+ARTIFACT_FILE=katana-linux-amd64
+INTOTO_ATTESTATION=katana-linux-amd64.intoto.jsonl
+
+wget https://${SOURCE_URI}/releases/download/${SOURCE_TAG}/${ARTIFACT_FILE}
+wget https://${SOURCE_URI}/releases/download/${SOURCE_TAG}/${INTOTO_ATTESTATION}
+
+~/go/bin/slsa-verifier verify-artifact $ARTIFACT_FILE \
+  --provenance-path $INTOTO_ATTESTATION \
+  --source-uri $SOURCE_URI \
+  --source-tag $SOURCE_TAG \
+  --print-provenance
