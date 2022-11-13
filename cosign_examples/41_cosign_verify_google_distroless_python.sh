a0b310dead8611ad400a98a7cdabd865a80fec43
Grig Gheorghiu <grig@gheorghiu.net>
2022-11-13 17:23:05 +0000

    python specific test

---
+++ cosign_examples/41_cosign_verify_google_distroless_python.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+GOOGLE_COSIGN_PUB_KEY=google_cosign.pub
+rm -rf cosign.pub
+wget https://raw.githubusercontent.com/GoogleContainerTools/distroless/main/cosign.pub
+mv cosign.pub $GOOGLE_COSIGN_PUB_KEY
+
+GOOGLE_DISTROLESS_IMAGES='
+gcr.io/distroless/python3-debian11'
+
+TAGS='
+latest
+nonroot
+debug
+debug-nonroot'
+
+for IMAGE in $GOOGLE_DISTROLESS_IMAGES; do
+	for TAG in $TAGS; do
+		echo Verifying $IMAGE:$TAG
+		cosign verify --key $GOOGLE_COSIGN_PUB_KEY $IMAGE:$TAG
+		echo
+	done
+done
+
+# found an issue!
+#Verifying gcr.io/distroless/python3-debian11:debug
+#Error: no matching signatures:
+#
+#main.go:62: error during command execution: no matching signatures:
