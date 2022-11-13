955cd6e5194be2740b8799d9b54d5343338f8659
Grig Gheorghiu <grig@gheorghiu.net>
2022-11-13 01:14:17 +0000

    fix bash script

--- image_history_inspect_examples/3_nerdctl_image_inspect.sh
+++ image_history_inspect_examples/3_nerdctl_image_inspect.sh
@@ -6,9 +6,6 @@ for IMAGE in $IMAGES; do
 	echo Running: nerdctl inspect $IMAGE
 	nerdctl inspect $IMAGE
 done
-#!/bin/bash
-
-IMAGES=$(nerdctl images | grep -v REPO | awk '{printf "%s:%s\n",$1, $2 }')
 
 for IMAGE in $IMAGES; do
 	echo Running: nerdctl history $IMAGE
