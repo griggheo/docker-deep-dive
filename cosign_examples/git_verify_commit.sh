970ca9d8d434011face7d5426a306fb71b4078b7
Grig Gheorghiu <grig@gheorghiu.net>
2022-11-15 01:59:21 +0000

    add git sign verification example

---
+++ cosign_examples/git_verify_commit.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git verify-commit 5d6f9d7
+#tlog index: 7095948
+#gitsign: Signature made using certificate ID 0x39afec2fa1626bde9b24f634ccfaa37a83e88e14 | CN=sigstore-intermediate,O=sigstore.dev
+#gitsign: Good signature from [grig@gheorghiu.net]
+#Validated Git signature: true
+#Validated Rekor entry: true
+
