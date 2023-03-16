bfd1ca1cc8c40089792948f2252844bbdd0fad51
Grig Gheorghiu <grig@gheorghiu.net>
2023-03-16 04:46:27 +0000

    add link to distroless article

--- distroless_examples/run_distroless_images.sh
+++ distroless_examples/run_distroless_images.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# root shell!
 docker run --rm -it --entrypoint sh gcr.io/distroless/python3-debian11
+
+# See https://www.form3.tech/engineering/content/exploiting-distroless-images
 docker run --rm -it --entrypoint openssl gcr.io/distroless/java17-debian11
