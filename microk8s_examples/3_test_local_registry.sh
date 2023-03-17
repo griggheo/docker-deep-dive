a90809ad8073d2f754563c4c39d8d877812ade96
Grig Gheorghiu <grig@gheorghiu.net>
2023-03-17 01:50:03 +0000

    use crane to push to local registry

--- microk8s_examples/3_test_local_registry.sh
+++ microk8s_examples/3_test_local_registry.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-nerdctl pull gcr.io/google-samples/hello-app:1.0
-nerdctl tag gcr.io/google-samples/hello-app:1.0 127.0.0.1:32000/hello-app:1.0
-sudo nerdctl push --insecure-registry 127.0.0.1:32000/hello-app:1.0
-sudo microk8s kubectl create deployment hello-server --image=127.0.0.1:32000/hello-app:1.0
+nerdctl pull --platform x86_64 gcr.io/google-samples/hello-app:2.0
+nerdctl tag gcr.io/google-samples/hello-app:2.0 127.0.0.1:32000/hello-app:2.0
+nerdctl image save 127.0.0.1:32000/hello-app:2.0 -o hello-app-2.0.tar
+
+crane push hello-app-2.0.tar 127.0.0.1:32000/hello-app:2.0
+crane ls 127.0.0.1:32000/hello-app
+crane digest
+crane manifest 127.0.0.1:32000/hello-app:2.0 | jq
+
+rm -rf hello-app-2.0.tar
+
+#sudo nerdctl push --insecure-registry 127.0.0.1:32000/hello-app:2.0
+sudo microk8s kubectl apply -f hello-server-deployment.yaml
