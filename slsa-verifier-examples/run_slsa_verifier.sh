8af696732bf4aee328a2ef55935faeedd39bc4ab
Grig Gheorghiu <grig@gheorghiu.net>
2023-03-18 17:48:29 +0000

    verify latest katana build

--- slsa-verifier-examples/run_slsa_verifier.sh
+++ slsa-verifier-examples/run_slsa_verifier.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 SOURCE_URI=github.com/codepraxis-io/katana
-#SOURCE_TAG=v1.0.0
-SOURCE_TAG=v1.0.1
+#SOURCE_TAG=v1.0.1
+SOURCE_TAG=v1.0.3
 ARTIFACT_FILE=katana-linux-amd64
 INTOTO_ATTESTATION=katana-linux-amd64.intoto.jsonl
 
+rm -rf $ARTIFACT_FILE $INTOTO_ATTESTATION
 wget https://${SOURCE_URI}/releases/download/${SOURCE_TAG}/${ARTIFACT_FILE}
 wget https://${SOURCE_URI}/releases/download/${SOURCE_TAG}/${INTOTO_ATTESTATION}
 
