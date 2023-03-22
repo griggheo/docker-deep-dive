96db11a48e8f7363c50aa2e1e213a5ced913cc14
Grig Gheorghiu <grig@gheorghiu.net>
2023-03-22 00:31:24 +0000

    verify latest image

--- cosign_examples/30_cosign_verify_keyless_signing.sh
+++ cosign_examples/30_cosign_verify_keyless_signing.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-DIGEST=ghcr.io/codepraxis-io/keyless-cosign@sha256:928d9b771f083c0722ca58ae2a551c48e346aa9df74b84f3b5829b79b74fc79b
+#DIGEST=ghcr.io/codepraxis-io/keyless-cosign@sha256:928d9b771f083c0722ca58ae2a551c48e346aa9df74b84f3b5829b79b74fc79b
+DIGEST=ghcr.io/codepraxis-io/keyless-cosign@sha256:6b7cdd9860d9319be7e3bbf807e219baa919413947862c0abed8fbc0fe9f975f
 COSIGN_EXPERIMENTAL=true cosign verify $DIGEST | jq .
 
