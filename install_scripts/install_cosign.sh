038a45b49c5aa95423d892039ac60c09dc524f88
Grig Gheorghiu <grig@gheorghiu.net>
2023-03-28 02:21:19 +0000

    download sbom produced by ko

--- install_scripts/install_cosign.sh
+++ install_scripts/install_cosign.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-COSIGN_VERSION=1.12.1
+COSIGN_VERSION=2.0.0
 
 wget "https://github.com/sigstore/cosign/releases/download/v${COSIGN_VERSION}/cosign-linux-amd64" 
 sudo mv cosign-linux-amd64 /usr/local/bin/cosign 
