e9dd06bf0f9120157aed79de4a1116994ea190b6
Grig Gheorghiu <grig@gheorghiu.net>
2023-05-30 23:01:33 +0000

    use tsv format for jq output

--- scan_result_parsing_and_sbom_examples/trivy_scan_sbom_spdx.sh
+++ scan_result_parsing_and_sbom_examples/trivy_scan_sbom_spdx.sh
@@ -14,22 +14,22 @@ cat $REPORT | jq '.Results[0].Vulnerabilities[].VulnerabilityID'
 echo
 
 echo CVE + URL
-cat $REPORT | jq '.Results[0].Vulnerabilities[]|.VulnerabilityID,.PrimaryURL'
+cat $REPORT | jq '.Results[0].Vulnerabilities[]' | jq  -r '[.VulnerabilityID,.PrimaryURL]|@tsv'
 echo
 
 echo CVE + URL + Title
-cat $REPORT | jq '.Results[0].Vulnerabilities[]|.VulnerabilityID,.PrimaryURL,.Title'
+cat $REPORT | jq '.Results[0].Vulnerabilities[]' | jq -r '[.VulnerabilityID,.PrimaryURL,.Title]|@tsv'
 echo
 
 echo CVE + severity
-cat $REPORT | jq '.Results[0].Vulnerabilities[]|.VulnerabilityID,.Severity'
+cat $REPORT | jq '.Results[0].Vulnerabilities[]' | jq -r '[.VulnerabilityID,.Severity]|@tsv'
 echo
 
 echo CVE + severity + CVSS NVD v3 score
 # See https://nvd.nist.gov/vuln-metrics/cvss
-cat $REPORT | jq '.Results[0].Vulnerabilities[]|.VulnerabilityID,.Severity,.CVSS.nvd.V3Score'
+cat $REPORT | jq '.Results[0].Vulnerabilities[]' | jq -r '[.VulnerabilityID,.Severity,.CVSS.nvd.V3Score]|@tsv'
 echo
 
 echo Select records with severity score greater than 7
-cat $REPORT | jq '.Results[0].Vulnerabilities[]|select(.CVSS.nvd.V3Score>7)' | jq '.VulnerabilityID,.PrimaryURL,.Title,.Severity,.CVSS.nvd.V3Score'
+cat $REPORT | jq '.Results[0].Vulnerabilities[]|select(.CVSS.nvd.V3Score>7)' | jq -r '[.VulnerabilityID,.PrimaryURL,.Title,.Severity,.CVSS.nvd.V3Score]|@tsv'
 echo
